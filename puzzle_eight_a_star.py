3e4adcbe0d693f23ceb3a641a1d064bddf320915
Pratik Shah <67506267+pratikiiitv@users.noreply.github.com>
2024-09-04 11:50:00 +0530

    Half cooked code for A*, BFS updated
    Notes updated on 4th Sep. Topic covered: Heuristic function by relaxing the problem

--- puzzle_eight_a_star.py
+++ puzzle_eight_a_star.py
@@ -1,9 +1,11 @@
-from collections import deque
+# from collections import deque
+import heapq
+# heapq for prioritizing on "g"
 import numpy as np
 import random
 
 # Author : Pratik Shah
-# Date : August 20, 2024
+# Date : Sept 4, 2024
 # Place : IIIT Vadodara
 
 # Course : CS307 Artificial Intelligence
@@ -11,9 +13,18 @@ import random
 # Learning Objective : Revisit concepts of basic data structures, BFS and DFS
 
 class Node:
-    def __init__(self, state, parent=None):
+    def __init__(self, state, parent=None, g=0, h=0):
         self.state = state
         self.parent = parent
+        self.g = g # distance to root
+        self.h = # estimated distance to goal
+        self.f = g + h # evaluation function
+    def __lt__(self, other):
+        return self.g < other.g
+
+def heuristic(node, goal_state):
+    h = 0
+    return h
 
 def get_successors(node):
     successors = []
@@ -44,37 +55,47 @@ def get_successors(node):
             temp = new_state[im]
             new_state[im] = new_state[index]
             new_state[index] = temp
-            successor = Node(new_state, node)
+            successor = Node(new_state, node, node.g+1)
+            print(successor.g)
             successors.append(successor)            
     return successors
 
-def bfs(start_state, goal_state):
+def search_agent(start_state, goal_state):
     start_node = Node(start_state)
     goal_node = Node(goal_state)
-    queue = deque([start_node])
+    # queue = deque([start_node])
+    frontier = []
+    heapq.heappush(frontier, (start_node.g, start_node))
+    # Push start_node to frontier (priority queue) with priority on "g"
     visited = set()
     nodes_explored = 0
-    while queue:
-        node = queue.popleft()
+    
+    while frontier:
+        _, node = heapq.heappop(frontier)
+        # Pop prioritized node from frontier
         if tuple(node.state) in visited:
             continue
         visited.add(tuple(node.state))
         # print(node.state)
-        nodes_explored = nodes_explored + 1
+        # nodes_explored = nodes_explored + 1
         if node.state == list(goal_node.state):
             path = []
             while node:
                 path.append(node.state)
                 node = node.parent
-            print('Total nodes explored', nodes_explored)
+            # print('Total nodes explored', nodes_explored)
             return path[::-1]
         for successor in get_successors(node):
-            queue.append(successor)
+            # queue.append(successor)
+            # push scucessor with updated "g" value in frontier
+            heapq.heappush(frontier, (successor.g, successor))
     # print('Total nodes explored', nodes_explored)
     return None
 
 start_state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 s_node = Node(start_state)
+# goal_state = [1, 2, 3, 4, 5, 0, 7, 8, 6]
+
 D = 20
 d = 0
 while d <= D:
@@ -83,7 +104,7 @@ while d <= D:
     d = d+1
     # print(goal_state)
 
-solution = bfs(start_state, goal_state)
+solution = search_agent(start_state, goal_state)
 if solution:
     print("Solution found:")
     for step in solution:
